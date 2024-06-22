d7f7314eeb9438668abd509b3432a5264a1da39a
joseph skokan <joeskokan20@gmail.com>
2024-06-21 20:14:34 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'about/index'
+  get 'pages/home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "pages#home"
+  get 'about', to: 'about#index', as: 'about'
+  get 'tome', to: 'pages#tome', as: 'tome'
+end
